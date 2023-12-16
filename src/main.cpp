3bc54221030915c3de5df5ce100a0465d67d9865
Ztirom45 <moritz.busse.mb@web.de>
2023-12-16 14:00:06 +0100

    simplify movement code

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ MeGyro gyro;
   digitalWrite(pinMotorLF, LOW);\
   digitalWrite(pinMotorLB, LOW);\
   digitalWrite(pinMotorRF, LOW);\
-  digitalWrite(pinMotorLB, LOW);\
+  digitalWrite(pinMotorRB, LOW);\
 }
 
 
@@ -37,90 +37,44 @@ class for advanced robot movement
   
   make sure, to reset move_finish via prepare_move bofore a moving
 */
-class Chassis{
-  public:
-    bool is_moving = false;
-    bool move_finish = false;
-    int last_angle = 0;
-    int last_time = 0;
-    MeGyro *ptrGyro;
+void move_angle(int angle,int speed,MeGyro* ptrGyro){
+  if(angle>0){//right
+    move(speed,-speed);
+  }else{//left
+    move(-speed,speed);
+  }
 
-    Chassis(MeGyro *ptrGyroInp): ptrGyro(ptrGyroInp){
-    };
-
-    inline void begin(){  
-      pinMode(LED_BUILTIN, pinMotorLF);
-      pinMode(LED_BUILTIN, pinMotorLB);
-      pinMode(LED_BUILTIN, pinMotorRF);
-      pinMode(LED_BUILTIN, pinMotorRB);
-    };
-
-    void move_angle(int speed,int angle){
-      if(this->move_finish){return;}
-      
-      if(angle==0){return;}
-      
-      gyro.update();
+  Serial.read();
+  gyro.update();
+  int last_angle = gyro.getAngleZ();
+  
+  if(angle>0){//right
+    int angle_now = ptrGyro->getAngleZ() - ((last_angle-angle<-180)? 360 : 0);
+    while(last_angle-angle < angle_now){
       Serial.read();
-      if(!this->is_moving){
-        this->last_angle = this->ptrGyro->getAngleZ();
-        if(angle>0){//right
-          move(speed,-speed);
-        }else{//left
-          move(-speed,speed);
-        }
-        this->is_moving = true;
-      }
-      
-      if(angle>0){//right
-        int angle_now = this->ptrGyro->getAngleZ()- ((this->last_angle-angle<-180)? 360 : 0);
-        if(this->last_angle-angle > angle_now){
-          stop();
-          this->move_finish = true;
-          this->is_moving = false;
-        }
-        return;
-        
-      }
-      //else left
-      int angle_now = this->ptrGyro->getAngleZ()+ ((this->last_angle+angle>180)? 360 : 0);
-      
-      if(this->last_angle-angle < angle_now){
-        stop();
-        this->move_finish = true;
-        this->is_moving = false;
-      }
-      return;
-
-    };
-    void move_time(int left,int right,int ms){
-      if(this->move_finish || ms<0){return;}
-       
-      if(!this->is_moving){
-        this->last_time = millis();
-        is_moving = true;
-        move(left,right);
-      }
-      
-      if(ms<millis()-last_time){
-        stop();
-        this->is_moving = false;
-        this->move_finish = true;
-      }
-      
-      
-    };
-
-    void prepare_move(){
-      this->is_moving = false;
-      this->move_finish = false;
+      gyro.update();
+      angle_now = ptrGyro->getAngleZ() - ((last_angle-angle<-180)? 360 : 0);
+      Serial.print("X: ");
+      Serial.println(angle_now);
+      //Serial.println(gyro.getAngleZ());
     }
-      
-      
+    stop();
+    return;
+  }
+  
+  //else left
+  int angle_now = ptrGyro->getAngleZ()+ ((last_angle+angle>180)? 360 : 0);
+  while(last_angle-angle > angle_now){  
+    Serial.read();
+    gyro.update();
+    angle_now = ptrGyro->getAngleZ()+ ((last_angle+angle>180)? 360 : 0);
+    Serial.print("X: ");
+    Serial.println(angle_now);
 
-};
-
-Chassis chassis(&gyro);
+    
+  }
+  stop();
+}
 
 void setup() {
 
@@ -128,27 +82,14 @@ void setup() {
   Serial.begin(9600);
   Serial.print("hi2\n");
   pinMode(LED_BUILTIN, OUTPUT);
-  //init sensors
+  //init sensors and motors
   gyro.begin();
-  chassis.begin();
-
-  chassis.prepare_move();
-  chassis.move_angle(100,90);
-  while(!chassis.move_finish){
-    Serial.print("X: ");
-    Serial.print(gyro.getAngleX());
-    Serial.print(" Y: ");
-    Serial.print(gyro.getAngleY());
-    Serial.print(" Z : ");
-    Serial.println(gyro.getAngleZ());
-    chassis.move_angle(100,90);
-  }
-  chassis.prepare_move();
-  chassis.move_time(100,100,2000);
-  while(!chassis.move_finish){
-    Serial.println(millis());
-    chassis.move_time(100,100,2000);
-  }
+  stop();
+  
+  //gyro test
+  move_angle(90,100,&gyro);
+  delay(1000);
+  move_angle(-45,100,&gyro);
 }
 
 // the loop function runs over and over again forever
